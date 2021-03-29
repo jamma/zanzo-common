7cfad772458fddb20f20c7e88e901114511a9f18
Michael McClenney <jamma@users.noreply.github.com>
2021-03-28 20:02:04 -0400

    Zanzo Common Feedback Changes
    Changes:
    - Changed a few things in response to Zanzo Common code review feedback

--- unity/Common/ZanzoObject.cs
+++ unity/Common/ZanzoObject.cs
@@ -1,18 +1,26 @@
 // +-------------------------------------------------------------------------------------------------------------------
 // + File: ZanzoObject.cs
 // + Company: Zanzo Studios - http://zanzostudios.com
-// + Author: Michael McClenney at 07:39 on 2021/03/08
+// + Author: Michael McClenney at 11:25 on 2021/03/27
 // +
 // + Description:
 // +    This base class has functionality to make managing things a bit easier.
-// +    The functionality is there so that it can be easily used within a ZanzoObjectManager,
+// +    The functionality is there so that it can easily be used within a ZanzoObjectManager,
 // +    but this should still be the base class for all components in a project whether or not they're managed.
 // +
-// +    1.  All ZanzoObjects have a flag that marks whether or not they're currently retained by a Manager.
+// +    1.  The methods Initialize() and Reinitialize() have similar names, but slightly different purposes.
+// +        If implemented, Initialize() should be called to complete proper set up of the component.
+// +        This call is expected to be called once ONLY, either immediately or soon after instantiation.
+// +
+// +        Reinitialize() should be called to reset the state of the component for re-use. This method can be called
+// +        whenever and however often as necessary. Ideally this method would be called Reset(), but that would clash
+// +        with a Unity MonoBehaviour life-cycle method of the same name.
+// +
+// +    2.  All ZanzoObjects have a flag that marks whether or not they're currently retained by a Manager.
 // +        This is done completely out of convenience, as it's far simpler to just query the object in
 // +        question to see whether or not it's currently retained, than to check with the object's manager.
 // +
-// +    2.  All ZanzoObjects have a flag that handles the concept of activate/inactive,
+// +    3.  All ZanzoObjects have a flag that handles the concept of activate/inactive,
 // +           which abstracts the concept of active/inactive from its specific meaning in Unity.
 // +        It makes perfect sense why Unity implemented and named it this way, but I frequently
 // +        want to consider something "deactivated" while still displaying it on-screen.
@@ -23,12 +31,11 @@
 // +        Unity gameObject.SetActive() method. These methods can be overridden to adapt to the needs
 // +        of a particular subclass.
 // +
-// +
-// +    3.  All ZanzoObjects emit events based on the above states:
+// +    4.  All ZanzoObjects emit events based on the above states:
 // +            3. Activated
 // +            4. Deactivated
 // +
-// +    4.  The expected lifecycle (order of events) for a typical ZanzoObject being used within a manager is:
+// +    5.  The expected lifecycle (order of events) for a typical ZanzoObject being used within a manager is:
 // +            1. Retained
 // +            2. Activated
 // +            3. Deactivated
@@ -42,35 +49,23 @@ using UnityEngine;
 
 namespace Zanzo.Common
 {
-    // +-------------------------------------------------------------------------------
+    // +---------------------------------------------------------------------------------------------------------------
     // + Class: ZanzoObject
     // + Description:
     // +    Insert Description Here
-    // +-------------------------------------------------------------------------------
+    // +---------------------------------------------------------------------------------------------------------------
     public class ZanzoObject : MonoBehaviour, Retainable
     {
-        // +---------------------------------------------------
-        // + Events & Delegates
-        // +---------------------------------------------------
+        // Events & Delegates  ----------------------------------------------------------------------------------------
         public delegate void ZanzoObjectNotify(ZanzoObject res);
         public event ZanzoObjectNotify Activated;
         public event ZanzoObjectNotify Deactivated;
 
-        // +-----------------------------------------------------------------------
-        // + Data Members
-        // +-----------------------------------------------------------------------
-        // +---------------------------------------------------
-        // + Private Members
-        // +---------------------------------------------------
+        // Private Members  -------------------------------------------------------------------------------------------
         private bool _isActivated = false;
         private bool _isRetained = false;
 
-        // +---------------------------------------------------
-        // + Public Members
-        // +---------------------------------------------------
-        // +-------------------------------
-        // + Properties
-        // +-------------------------------
+        // Properties  ------------------------------------------------------------------------------------------------
         public bool IsActive
         {
             get
@@ -87,19 +82,12 @@ namespace Zanzo.Common
             }
         }
 
-        // +-----------------------------------------------------------------------
-        // + Class Methods
-        // +-----------------------------------------------------------------------
-        // +---------------------------------------------------
-        // + C'tor & Init Methods
-        // +---------------------------------------------------
-        public virtual void Initialize()
-        {
-        }
+        // C'tor & Init Methods  --------------------------------------------------------------------------------------
+        public virtual void Initialize() {}
+        public virtual void Reinitialize() {}
 
-        // +---------------------------------------------------
-        // + Component Functionality
-        // +---------------------------------------------------
+        // Component Functionality  -----------------------------------------------------------------------------------
+
         // NOTE: My gut tells me that Retain / Release / Activate / Deactivate should NOT be virtual,
         //       but if the need arises reconsider this. Currently, I cannot imagine a scenario where it makes sense.
         //       Also, and probably more importantly, Retain / Release should probably only be callable to classes
@@ -136,49 +124,5 @@ namespace Zanzo.Common
         {
             gameObject.SetActive(false);
         }
-
-        public virtual void Reset()
-        {
-        }
-
-        // +---------------------------------------------------
-        // + Unity Life-Cycle Methods
-        // + Order: https://docs.unity3d.com/Manual/ExecutionOrder.html
-        // +---------------------------------------------------
-        // void Awake()
-        // {
-        // }
-
-        // void OnEnable()
-        // {
-        // }
-
-        // void OnDisable()
-        // {
-        // }
-
-        // void Start()
-        // {
-        // }
-
-        // void Update()
-        // {
-        // }
-
-        // void FixedUpdate()
-        // {
-        // }
-
-        // void LateUpdate()
-        // {
-        // }
-
-        // void OnApplicationQuit()
-        // {
-        // }
-
-        // void OnDisable()
-        // {
-        // }
     }
 }
